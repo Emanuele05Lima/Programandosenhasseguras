2a552c4783c12d9078ee1ff22b6539d073b2bb5d
Emanuele05Lima <krassowski.emanuele@escola.pr.gov.br>
2026-07-09 09:26:43 -0300

    .

--- main.js
+++ main.js
@@ -29,3 +29,9 @@ function aumentaTamanho(){
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
+
+for (i = 0; i < checkbox.length; i++) {
+    checkbox[i].onclick = geraSenha;
+}
+
+geraSenha();
